dc873631b1386c2867db561581afc2a354933a44
USER-11 <USER-11@LAB06-PC11>
2017-06-30 15:33:15 -0500

    ya1

--- hola/src/hola/Hola.java
+++ hola/src/hola/Hola.java
@@ -17,6 +17,7 @@ public class Hola {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("se cambio? confirmaa");
+        System.out.println("Hola");
     }
     
 }
